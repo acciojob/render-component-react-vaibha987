899e8c75f9d0cc602cda119dccf505a1df938ff7
Sahil jangid <saahil.jangid2611@gmail.com>
2024-10-06 21:25:35 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <p>I am learning React. My life is getting better</p>
+    </div>
   )
+  React.DOM.render(<App/>,document.getElementById("main"))
 }
 
 
